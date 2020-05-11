4ee8b7f96c8b3f0c657dd5235a1a839278567d25
Alexander Chaschin <ac@happynumbers.com>
2020-05-11 18:06:59 +0300

    extract bank account

--- models/bank/bank_account.rb
+++ models/bank/bank_account.rb
@@ -29,27 +29,3 @@ class Account
     other.add(amount)
   end
 end
-
-# Bank and money pool for bets
-class Bank
-  attr_reader_writer :player, :dealer, :pool
-
-  def initialize(start_money)
-    @player = Account.new(:player, start_money)
-    @dealer = Account.new(:dealer, start_money)
-    @pool = Account.new(:pool, 0)
-  end
-
-  def place_bet(amount)
-    pool.from(player, amount)
-    pool.from(dealer, amount)
-  end
-
-  def player_won
-    player.from(pool)
-  end
-
-  def dealer_won
-    dealer.from(pool)
-  end
-end
