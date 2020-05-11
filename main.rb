ddbbc6736187de5ad36138872208c21b7f3e3d69
Alexander Chaschin <ac@happynumbers.com>
2020-05-11 14:53:51 +0300

    add kernel extension

--- main.rb
+++ main.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'models/card.rb'
-require_relative 'models/card_hand.rb'
-require_relative 'models/bank.rb'
-require_relative 'models/deck.rb'
-require_relative 'models/player.rb'
-require_relative 'modules/game_initializer.rb'
-require_relative 'game.rb'
+require_relative 'kernel.rb'
+require_all '.'
 
 Game.new.run
