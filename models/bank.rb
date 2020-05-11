552deac56b0e28db8e8a9c1cb9ffdebfcfb476f5
Alexander Chaschin <ac@happynumbers.com>
2020-05-11 15:08:54 +0300

    bank class

--- models/bank.rb
+++ models/bank.rb
@@ -1,4 +1,35 @@
+# Bank and money pool for bets
 class Bank
+  attr_reader :accounts, :pool
+
   def initialize
+    @accounts = []
+    @pool = 0
   end
+
+  def add_account(name, amount)
+    raise "Already created account: #{name}" if accounts[name]
+
+    accounts[name] = amount
+  end
+
+  def to_pool(name, amount)
+    money_after = accounts[name] - amount
+    raise "#{name} can't give #{amount}" if money_after.negative?
+
+    accounts[name] = money_after
+    self.pool += amount
+  end
+
+  def from_pool(name, amount)
+    money_after = pool - amount
+    raise "Pool don't have #{amount}" if money_after.negative?
+
+    accounts[name] += amount
+    self.pool = money_after
+  end
+
+  protected
+
+  attr_writer :accounts, :pool
 end
