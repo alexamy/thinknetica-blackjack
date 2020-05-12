66393e92d7568c852563ad2f4a66583efc3b72af
Alexander Chaschin <ac@happynumbers.com>
2020-05-13 01:44:36 +0300

    add delegate class method

--- extensions/class.rb
+++ extensions/class.rb
@@ -6,11 +6,23 @@ class Class
     end
   end
 
+  def delegate(*methods, to:)
+    methods.each { |method| gelegate_one(method, to) }
+  end
+
   protected
 
   def add_attr_reader_writer(key)
     attr_reader key
     attr_writer key
     protected "#{key}="
+  end
+
+  def gelegate_one(method, target)
+    var_name = "@#{target}"
+    define_method method do |*args|
+      var = instance_variable_get(var_name)
+      var.send(method, *args)
+    end
   end
 end
