999409d7267c2cd619f0f8f2fd2dd08c91888e87
Alexander Chaschin <ac@happynumbers.com>
2020-05-13 00:03:08 +0300

    refactor attr_reader_writer

--- extensions/class.rb
+++ extensions/class.rb
@@ -1,19 +1,26 @@
 # Class extensions
 class Class
   def attr_reader_writer(*keys)
-    keys.each { |key| add_attr_reader_writer(key) }
+    keys.each do |key|
+      add_attr_reader(key)
+      add_attr_writer(key)
+    end
   end
 
   protected
 
-  def add_attr_reader_writer(key)
-    get = key
-    set = "#{key}="
+  def add_attr_reader(key)
     var = "@#{key}"
+    get = key
 
     define_method(get) do
       instance_variable_get(var)
     end
+  end
+
+  def add_attr_writer(key)
+    var = "@#{key}"
+    set = "#{key}="
 
     define_method(set) do |value|
       instance_variable_set(var, value)
