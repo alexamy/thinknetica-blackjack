80358f8706219e131d17770cf1c7af35c5202541
Alexander Chaschin <ac@happynumbers.com>
2020-05-17 14:56:15 +0300

    add game

--- game.rb
+++ game.rb
@@ -1,7 +1,24 @@
+# Main entry
 class Game
+  attr_reader_writer :players
+
+  START_MONEY = 100
+
   def initialize
+    @players = {
+      user: Player.new(:user, START_MONEY),
+      dealer: Player.new(:dealer, START_MONEY)
+    }
   end
 
   def run
+    show_ui
+    ask_choice
+  end
+
+  def show_ui
+  end
+
+  def ask_choice
   end
 end
