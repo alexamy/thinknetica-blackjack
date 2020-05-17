019814f91ee676467dd08a8002333fd3518e10f1
Alexander Chaschin <ac@happynumbers.com>
2020-05-17 16:28:05 +0300

    add ui width adjust

--- game.rb
+++ game.rb
@@ -27,12 +27,13 @@ class Game
   def show_ui
     players.each do |name, player|
       cards = player.cards
-      cards_str = cards.map(&:to_s).join(' ')
 
-      cards_row = "#{cards_str}  Σ #{Card.points(cards)}"
-      bank_row = "#{name.to_s.capitalize} #{player.money}$"
+      cards_str = cards.map(&:to_s).join(' ').ljust(10, ' ')
+      points = "Σ #{Card.points(cards)}".ljust(2, ' ')
+      name = name.to_s.rjust(8, ' ')
+      money = "#{player.money}$".ljust(3, ' ')
 
-      puts "#{cards_row} #{bank_row}", "\n"
+      puts [cards_str, points, name, money].join(' '), "\n"
     end
     puts "Bank #{pool}$", "\n"
   end
