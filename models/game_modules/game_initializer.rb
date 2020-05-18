b0c8e4771de32c1522f47ea07076f0f2c572f879
Alexander Chaschin <ac@happynumbers.com>
2020-05-18 21:35:33 +0300

    refactor game initalizer

--- models/game_modules/game_initializer.rb
+++ models/game_modules/game_initializer.rb
@@ -4,7 +4,7 @@ module Game
     START_MONEY = 100
 
     def init_players
-      @players = [:user, :dealer].map do |name|
+      @players = %i[user dealer].map do |name|
         player = Player.new(name, START_MONEY)
         self.class.send(:define_method, name) { player }
         player
@@ -12,11 +12,21 @@ module Game
     end
 
     def init_session
+      init_deck
+      init_pool
+    end
+
+    protected
+
+    def init_deck
       self.deck = Deck.new
       players.each(&:throw_cards)
       2.times { players.each { |player| player.add_card(deck.get) } }
+    end
 
+    def init_pool
       raise NoMoney.new if players.all? { |player| player.money < 10 }
+
       self.pool = players.map { |player| player.get_money(10) }.sum
     end
   end
