946f4975622f755df762cdc2f5b44d410627b8d2
Alexander Chaschin <ac@happynumbers.com>
2020-05-19 01:29:03 +0300

    comments for events

--- models/events.rb
+++ models/events.rb
@@ -1,6 +1,8 @@
 # session must end
 class EndSession < StandardError; end
+
 # user ask for a new game
 class NewGame < StandardError; end
-# end game
+
+# end game if no money left
 class NoMoney < StandardError; end
