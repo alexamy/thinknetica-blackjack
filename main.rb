d501fe4d1c0dc89dd20d276faaa3b8b4c5b0686d
Alexander Chaschin <chaschin@uchi.ru>
2020-09-14 11:53:40 +0300

    remove require_all

--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
-require_relative 'extensions/require_all.rb'
-require_all '.'
+require_relative 'extensions/hash.rb'
+require_relative 'extensions/class.rb'
+require_relative 'models/modules/card_constants.rb'
+require_relative 'models/modules/player_print.rb'
+require_relative 'models/game_modules/game_loop.rb'
+require_relative 'models/game_modules/game_initializer.rb'
+require_relative 'models/game_modules/game_ui.rb'
+require_relative 'models/game_modules/game_rules.rb'
+require_relative 'models/card/card.rb'
+require_relative 'models/card/deck.rb'
+require_relative 'models/player.rb'
+require_relative 'models/events.rb'
+require_relative 'models/game.rb'
 
 puts Game::UI.intro, "\n"
 Game::Runner.new.run
